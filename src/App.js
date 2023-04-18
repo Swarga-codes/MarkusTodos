385b1eaa447494e237102cfc5ecc856d373aaddf
Swarga-codes <sd3526@srmist.edu.in>
2023-04-19 04:13:36 +0530

    added add todo functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import { useState } from 'react';
 import './App.css';
 import TodoList from './TodoList';
-import TodoInput from './TodoInput';
+
 
 function App() {
+
 const[todosList,setTodosList]=useState([]);
-const[todos,setTodos]=useState({title:"",description:"",isDone:false});
+const[title,setTitle]=useState("");
+const[desc,setDesc]=useState("");
+const addTodo=(t,d)=>{
+
+  setTodosList([...todosList,{title:t,desc:d}])
+
+}
   return (
     <div className="App">
     <h1>Let's build Todo app!</h1>
-    <TodoInput/>
+    <div className='todos_input'>
+    <div className='Title'>
+    <p>Title</p>
+    <input type="text" name='title' value={title} onChange={(e)=>{setTitle(e.target.value)}
+  }/>
+    </div>
+    <div className='Description'>
+    <p>Description</p>
+    <input type="text" name='description' value={desc} onChange={(e)=>{setDesc(e.target.value);
+  }}/>
+    </div>
+    <button onClick={()=>addTodo(title,desc)}>Add Todo</button>
+    </div>
       <div className="todo_lists">
-   
+ {todosList.map(todo=>
+  (
+    <>
+    <h1>{todo.title}</h1>
+    <p>{todo.desc}</p>
+    </>
+  ))}
       </div>
     </div>
   );
