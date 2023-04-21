16aa120de2edf1c1956050fb2047e2afeef056bc
Swarga-codes <sd3526@srmist.edu.in>
2023-04-21 12:24:35 +0530

    created user model

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,12 @@ const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
 const{mongoUrl}=require('./keys')
+const cors=require('cors')
 app.use(express.json())
-app.use(require('./routes/auth'))
+app.use(cors())
 mongoose.connect(mongoUrl);
+require('./models/users')
+app.use(require('./routes/auth'))
 mongoose.connection.on('connected',()=>{
     console.log('Connected to mongodb...')
 })
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,21 @@
 const express=require('express');
 const router=express.Router();
+const mongoose=require('mongoose')
+const USERS=mongoose.model('USERS')
+const cors=require('cors')
+router.use(cors())
 router.get('/about',(req,res)=>{
     res.send("Hello I am about")
+})
+router.post('/signup',(req,res)=>{
+    const{email,userName,password}=req.body;
+    if(!email || !userName || !password) return res.status(422).json({error:"One or more fields are missing"});
+    
+    USERS.findOne({$or:[{email:email},{userName:userName}]}).then((saved)=>{
+        if(saved){
+            return res.status(422).json({error:'User already exists'})
+        }
+        
+    })
 })
 module.exports=router;
