fb94180607f5b2d4ea784c2707ac528fbee5261f
Swarga-codes <sd3526@srmist.edu.in>
2023-04-28 20:13:45 +0530

    fetched login and signup in frontend

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.css'
-import { Link } from 'react-router-dom'
-function Login() {
+import { Link, useNavigate } from 'react-router-dom'
+function SignUp() {
+    const[email,setEmail]=useState('');
+    const[userName,setUserName]=useState('');
+    const[password,setPassword]=useState('');
+    const[message,setMessage]=useState('');
+    const navigator=useNavigate();
+    const fetchSignUp=async(e)=>{
+        e.preventDefault();
+        const res=await fetch('http://localhost:8000/signup',{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                email,
+                userName,
+                password
+            })
+        });
+        const data=await res.json();
+        if(data.error){
+            setMessage(data.error)
+        }else{
+            navigator('/login')
+        }
+    }
   return (
     <div className='Signup'>
-    <form action="">
+    <form onSubmit={fetchSignUp}>
     <h1>Sign Up</h1>
+    <p className='error_message'>{message}</p>
     <div className="signup_email">
-    <input type="email" required placeholder='Enter your email'/>
+    <input type="email" required placeholder='Enter your email' value={email} onChange={e=>setEmail(e.target.value)}/>
     </div>
     <div className="signup_username">
-    <input type="username" required placeholder='Enter your username'/>
+    <input type="username" required placeholder='Enter your username' value={userName} onChange={e=>setUserName(e.target.value)}/>
     </div>
     <div className="signup_password">
-    <input type="password" required placeholder='Enter your password'/>
+    <input type="password" required placeholder='Enter your password' value={password} onChange={e=>setPassword(e.target.value)}/>
     </div>
   <button type='submit'>Sign Up</button>
   <p className='existing_account'>Already have an account?<Link to='/login'>Login</Link></p>
@@ -22,4 +48,4 @@ function Login() {
   )
 }
 
-export default Login
+export default SignUp
